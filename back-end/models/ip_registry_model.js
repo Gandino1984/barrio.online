aa6924769f186a5b21c843f859fb6b895e8674c0
Gandino1984 <andinogerman@gmail.com>
2024-11-24 21:39:24 +0100

    ip track

---
+++ back-end/models/ip_registry_model.js
@@ -0,0 +1,23 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/sequelize.js';
+
+const IpRegistry = sequelize.define('ip_registry', {
+    ip_address: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false
+    },
+    registration_count: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    },
+    last_attempt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    tableName: 'ip_registry',
+    timestamps: false
+});
+
+export default IpRegistry;
