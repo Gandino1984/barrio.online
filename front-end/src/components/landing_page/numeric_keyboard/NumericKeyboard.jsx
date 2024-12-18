3725eebc751e66bc38e681deb8f2fc4ceab40799
Gandino1984 <andinogerman@gmail.com>
2024-12-18 14:06:10 +0100

    password icons display fixed

--- front-end/src/components/landing_page/numeric_keyboard/NumericKeyboard.jsx
+++ front-end/src/components/landing_page/numeric_keyboard/NumericKeyboard.jsx
@@ -32,8 +32,12 @@ const NumericKeyboard = ({
 
   useEffect(() => {
     if (showMaskedPassword) {
-      const newIcons = Array(value.length).fill().map(() => icons[Math.floor(Math.random() * icons.length)]);
-      setPasswordIcons(newIcons);
+      if (passwordIcons.length < value.length) {
+        const newIcons = Array(value.length - passwordIcons.length).fill().map(() => icons[Math.floor(Math.random() * icons.length)]);
+        setPasswordIcons([...passwordIcons, ...newIcons]);
+      } else if (passwordIcons.length > value.length) {
+        setPasswordIcons(passwordIcons.slice(0, value.length));
+      }
     } else {
       setPasswordIcons([]);
     }
@@ -64,59 +68,59 @@ const NumericKeyboard = ({
 
   return (
       <div className={styles.container}>
-          <div className={`${styles.passwordDisplay} ${error ? 'text-red-500' : ''}`}>
-              {displayedPassword}
-          </div>
-          <div className={styles.keyboard}>
-              <div className={styles.row}>
-                  {[1, 2, 3].map(num => (
+            <div className={`${styles.passwordDisplay} ${error ? 'text-red-500' : ''}`}>
+                {displayedPassword}
+            </div>
+
+            <div className={styles.keyboard}>
+                  <div className={styles.row}>
+                      {[1, 2, 3].map(num => (
+                          <button 
+                            key={num} 
+                            className={styles.key} 
+                            onClick={(e) => handleKeyClick(num.toString(), e)}
+                          >
+                              {num}
+                          </button>
+                      ))}
+                  </div>
+                  <div className={styles.row}>
+                      {[4, 5, 6].map(num => (
+                          <button 
+                            key={num} 
+                            className={styles.key} 
+                            onClick={(e) => handleKeyClick(num.toString(), e)}
+                          >
+                              {num}
+                          </button>
+                      ))}
+                  </div>
+                  <div className={styles.row}>
+                      {[7, 8, 9].map(num => (
+                          <button 
+                            key={num} 
+                            className={styles.key} 
+                            onClick={(e) => handleKeyClick(num.toString(), e)}
+                          >
+                              {num}
+                          </button>
+                      ))}
+                  </div>
+                  <div className={styles.row}>
                       <button 
-                        key={num} 
-                        className={styles.key} 
-                        onClick={(e) => handleKeyClick(num.toString(), e)}
+                        className={`${styles.key} ${styles.zero}`} 
+                        onClick={(e) => handleKeyClick('0', e)}
                       >
-                          {num}
+                          0
                       </button>
-                  ))}
-              </div>
-              <div className={styles.row}>
-                  {[4, 5, 6].map(num => (
                       <button 
-                        key={num} 
-                        className={styles.key} 
-                        onClick={(e) => handleKeyClick(num.toString(), e)}
+                        className={`${styles.key} ${styles.clear}`} 
+                        onClick={(e) => handleBackspaceClick(e)}
                       >
-                          {num}
+                            <Delete size={24} />
                       </button>
-                  ))}
-              </div>
-              <div className={styles.row}>
-                  {[7, 8, 9].map(num => (
-                      <button 
-                        key={num} 
-                        className={styles.key} 
-                        onClick={(e) => handleKeyClick(num.toString(), e)}
-                      >
-                          {num}
-                      </button>
-                  ))}
-              </div>
-              <div className={styles.row}>
-                 
-                  <button 
-                    className={`${styles.key} ${styles.zero}`} 
-                    onClick={(e) => handleKeyClick('0', e)}
-                  >
-                      0
-                  </button>
-                  <button 
-                    className={`${styles.key} ${styles.clear}`} 
-                    onClick={(e) => handleBackspaceClick(e)}
-                  >
-                        <Delete size={24} />
-                  </button>
-              </div>
-          </div>
+                  </div>
+            </div>
     </div>
   );
 };
