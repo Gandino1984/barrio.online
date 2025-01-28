0617b5a8d52a71a1c667ef3523bcefc8cfd34bc5
Gandino1984 <andinogerman@gmail.com>
2025-01-28 19:26:29 +0100

    css update

--- front-end/src/components/shop_management/shop_creation_form/ShopCreationForm.jsx
+++ front-end/src/components/shop_management/shop_creation_form/ShopCreationForm.jsx
@@ -24,80 +24,83 @@ const ShopCreationForm = () => {
 
   return (
     <div className={styles.container}>
-        <div className={styles.header}>   
-            <h3 className={styles.headerTitle}>
-                Crear un comercio
-            </h3>
-        </div>
-      <form onSubmit={handleAddShop} className={styles.form}>
-        <div className={styles.formField}>
-          <input
-            type="text"
-            placeholder='Nombre del comercio:'
-            value={newShop.name_shop}
-            onChange={(e) => setNewShop({...newShop, name_shop: e.target.value})}
-            className={styles.input}
-            required
-          />
-        </div>
-        
-        {/* Shop Type Dropdown */}
-        <div className={styles.formField}>
-          <select
-            value={newShop.type_shop}
-            onChange={(e) => {
-              // Reset subtype when type changes
-              setNewShop({
-                ...newShop, 
-                type_shop: e.target.value,
-                subtype_shop: '' // Clear subtype
-              })
-            }}
-            className={styles.input} 
-            required
-          >
-            <option value="" disabled>Categoría</option>
-            {shopTypes.map(type => (
-              <option key={type} value={type}>{type}</option>
-            ))}
-          </select>
-        </div>
+      <div className={styles.content}>
 
-        {/* Subtype Dropdown - Only show if a type is selected */}
-        {newShop.type_shop && (
-          <div className={styles.formField}>
-            <select
-              value={newShop.subtype_shop}
-              onChange={(e) => setNewShop({...newShop, subtype_shop: e.target.value})}
-              className={styles.input} 
-              required
-            >
-              <option value="" disabled>Subcategoría</option>
-              {subtypes.map(subtype => (
-                <option key={subtype} value={subtype}>{subtype}</option>
-              ))}
-            </select>
+          <div className={styles.header}>   
+              <h3 className={styles.headerTitle}>
+                  Crear un comercio
+              </h3>
           </div>
-        )}
-        
-        <div className={styles.formField}>
-          <input
-            type="text"
-            placeholder='Dirección del comercio:'
-            value={newShop.location_shop}
-            onChange={(e) => setNewShop({...newShop, location_shop: e.target.value})}
-            className={styles.input}
-            required
-          />
-        </div>
-        <button 
-          type="submit" 
-          className={styles.createButton}
-        >
-          Crear
-          <Box size={17} />
-        </button>
-      </form>
+          <form onSubmit={handleAddShop} className={styles.form}>
+            <div className={styles.formField}>
+              <input
+                type="text"
+                placeholder='Nombre del comercio:'
+                value={newShop.name_shop}
+                onChange={(e) => setNewShop({...newShop, name_shop: e.target.value})}
+                className={styles.input}
+                required
+              />
+            </div>
+            
+            {/* Shop Type Dropdown */}
+            <div className={styles.formField}>
+              <select
+                value={newShop.type_shop}
+                onChange={(e) => {
+                  // Reset subtype when type changes
+                  setNewShop({
+                    ...newShop, 
+                    type_shop: e.target.value,
+                    subtype_shop: '' // Clear subtype
+                  })
+                }}
+                className={styles.input} 
+                required
+              >
+                <option value="" disabled>Categoría</option>
+                {shopTypes.map(type => (
+                  <option key={type} value={type}>{type}</option>
+                ))}
+              </select>
+            </div>
+
+            {/* Subtype Dropdown - Only show if a type is selected */}
+            {newShop.type_shop && (
+              <div className={styles.formField}>
+                <select
+                  value={newShop.subtype_shop}
+                  onChange={(e) => setNewShop({...newShop, subtype_shop: e.target.value})}
+                  className={styles.input} 
+                  required
+                >
+                  <option value="" disabled>Subcategoría</option>
+                  {subtypes.map(subtype => (
+                    <option key={subtype} value={subtype}>{subtype}</option>
+                  ))}
+                </select>
+              </div>
+            )}
+            
+            <div className={styles.formField}>
+              <input
+                type="text"
+                placeholder='Dirección del comercio:'
+                value={newShop.location_shop}
+                onChange={(e) => setNewShop({...newShop, location_shop: e.target.value})}
+                className={styles.input}
+                required
+              />
+            </div>
+            <button 
+              type="submit" 
+              className={styles.createButton}
+            >
+              Crear
+              <Box size={17} />
+            </button>
+          </form>
+      </div>
     </div>
   );
 };
